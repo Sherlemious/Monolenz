6c7cf86f2d6d5f8b809de3dc3439027e3e0a9726
Abdelrahman Mohammed <abd.moh.yousef@gmail.com>
2025-07-23 01:51:35 +0300

    chore(setup): initialize turborepo with skeleton code for the monorepo

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+const Home: FC = () => {
+  return (
+    <div>
+      <main>
+        <h1>Welcome to Athaar</h1>
+        <p>This is your starting point. Begin building your application here.</p>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
---
+++ apps/web/eslint.config.js
@@ -0,0 +1,4 @@
+import { nextJsConfig } from '@athaar/eslint-config/next-js';
+
+/** @type {import("eslint").Linter.Config} */
+export default nextJsConfig;
